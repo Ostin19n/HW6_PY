82a823c93600e81312ae64e3a61c5805877eec9d
Anastasya <anastasiaostin@mail.ru>
2023-05-13 13:30:25 +0300

    Решение задачи

---
+++ 2_ex.py
@@ -0,0 +1,18 @@
+# Задача 32: Определить индексы элементов массива (списка), 
+# значения которых принадлежат заданному диапазону (т.е. не меньше заданного минимума и не больше заданного максимума)
+
+import random
+
+n = int(input('Введите количество элементов массива: '))
+list = []
+for i in range(n):
+    list.append(random.randint(-10, 10))
+print(list)
+
+min_num = int(input('Введите минимальный элемент диапазона: '))
+max_num = int(input('Введите максимальный элемент диапазона: '))
+list_new=[]
+for i in range(len(list)):
+    if min_num <= list[i] <= max_num:
+        list_new.append(i)
+print(list_new)
